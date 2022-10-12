3633a787a42c9ceb425d20d0b16e276efa207ad2
mustafabdi100 <mustafa300abdi.@gmail.com>
2022-10-12 06:26:48 -0400

    python exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_division(a, b):
-    res = 0
     try:
-        res = a / b
-    except Exception:
-        res = None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        print("Inside result: {}".format(res))
-    return 
+        print("Inside result: {}".format(div))
+    return (div)
+Footer
+© 2022 GitHub, Inc.
